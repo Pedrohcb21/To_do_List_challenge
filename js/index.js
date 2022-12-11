1a7e4067521f94be66300ff206d71960023e393c
Pedrohcb21 <pedrohcb21@gmail.com>
2022-12-11 16:17:50 -0300

    Adiconando no DOM

--- js/index.js
+++ js/index.js
@@ -2,9 +2,14 @@ let inserir = document.querySelector("form");
 inserir.addEventListener("submit" , (e) => {
     e.preventDefault();
 
-    let dados = obterInfo();
-    console.table(arrayMap);
+    let atividade = obterInfo();
+
+    if (atividade != false) {
+        atividades.push(atividade);
+    };
 });
+
+let atividades = Array();
 
 const obterInfo = () => {
     let todoInfo = document.querySelector("#todo-input").value;
@@ -13,11 +18,16 @@ const obterInfo = () => {
         alert (`Preencha todos os campos!`)
         return false
     } else {
-        let dados = {
+        let atividade = {
             todoInfo
         };
-        return dados;
-    }
+
+        inserir.reset();
+
+        addDom(atividade);
+        
+        return atividade;
+    };
 };
 
 const valida = (campo) => {
@@ -26,4 +36,41 @@ const valida = (campo) => {
     } else {
         return true;
     }
+};
+
+function addDom (atividade) {
+    //Texto da atividade
+    let nomeAtividade = document.createElement("li");
+    nomeAtividade.innerHTML = `${atividade.todoInfo}`;
+    nomeAtividade.className = "todo-item";
+
+    //Criação icone do botão excluir
+    let btnIconExcluir = document.createElement("i");
+    btnIconExcluir.className = "fas fa-trash";
+
+    //Criação botão excluir
+    let btnExcluir = document.createElement("button");
+    btnExcluir.appendChild(btnIconExcluir);
+    btnExcluir.className = "trash-btn";
+
+    //Criação do icone do botão de confirmar
+    let btnIconConfirm = document.createElement("i");
+    btnIconConfirm.className = "fas fa-check";
+
+    //Criação do botão confirmar
+    let btnConfirm = document.createElement("button");
+    btnConfirm.appendChild(btnIconConfirm);
+    btnConfirm.className = "check-btn";
+
+    //Criação da div das atvidades
+    let contAtividades  = document.createElement("div");
+    contAtividades.appendChild(nomeAtividade);
+    contAtividades.appendChild(btnConfirm);
+    contAtividades.appendChild(btnExcluir);
+    contAtividades.className = "todo";
+
+    //Adicionando a div e seu filhos a lista de atividades
+    let contList = document.querySelector("#todo-list");
+    contList.appendChild(contAtividades);
+    contList.className = "todo-list";
 };
